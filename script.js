c29a8c2fcb3d68f0e81ce65fc6a84914566299db
Latif5758 <latifikhwanulmuttaqin@gmail.com>
2022-08-31 06:06:25 +0700

    Adigment 1.1.0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var input = 1
+var input = 0
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement
@@ -125,9 +125,6 @@ class Calculator {
   
   operationButtons.forEach(button => {
     button.addEventListener('click', () => {
-      if(button.innerText === '%'){
-
-      }
       calculator.chooseOperation(button.innerText)
       input = 0
       calculator.updateDisplay()
@@ -149,4 +146,90 @@ class Calculator {
   deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
+  })
+
+  document.addEventListener('keydown', (event) =>{
+    let operand
+    let plusminus = 0
+    let equal = 0
+    let clear = 0
+    let allClear = 0
+    switch (event.key){
+      case '%':
+        operand = '%'
+      break
+      case '/':
+        operand = '/'
+      break
+      case '*':
+        operand = '*'
+      break
+      case '-':
+        operand = '-'
+      break
+      case '+':
+        operand = '+'
+      break
+      case '=':
+        equal = 1
+      break
+      case 'c':
+        clear = 1
+      break
+      case 'a':
+        allClear = 1
+      break
+      case 'C':
+        clear = 1
+      break
+      case 'A':
+        allClear = 1
+      break
+      case 'M':
+        plusminus = 1
+      break
+      case 'm':
+        plusminus = 1
+      break
+      default:
+      break
+    }
+    if (9 >= event.key || event.key === '.'){
+    if (input){
+      calculator.clear()
+      calculator.updateDisplay()
+      input = 0
+    }
+    calculator.appendNumber(event.key)
+    calculator.updateDisplay()}
+    else{
+      if(operand){
+        calculator.chooseOperation(operand)
+        input = 0
+        calculator.updateDisplay()
+      }   
+      if(equal){
+        calculator.compute()
+        calculator.updateDisplay()
+        input = 1
+        equal = 0
+      }
+      if(clear){
+        calculator.delete()
+        calculator.updateDisplay()
+        clear = 0
+      }
+      if(allClear){
+        calculator.clear()
+        calculator.updateDisplay()
+        input = 1
+        allClear = 0
+      }
+      if(plusminus){
+        calculator.chooseOperation('+/-')
+        input = 0
+        calculator.updateDisplay()
+        plusminus = 0
+      }
+    }
   })
